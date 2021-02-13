90fac815d97954761ac9298817015b5faf0d1dc7
Deyna Contreras <deyna.alexac@gmail.com>
2021-02-13 19:41:39 -0300

    Desafio blog casi terminado

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+       http_basic_authenticate_with name: 'desafiovamoscontodo', password: 'XAHTJEAS23123%23', except: :index
+       
+       def index
+              @posts = Post.all.order(created_at: :desc)
+       end
+
+       def new
+              @post = Post.new
+       end    
+
+       def create
+              censored_word = ["spoiler"]
+              @post = Post.new(post_params)
+              
+              if (@post.content.split).any? == censored_word.any?
+                     redirect_to posts_dashboard_path, notice: "No puedes utilizar la palabra spoiler"
+              elsif @post.save
+                     redirect_to root_path, notice: "El post fue ingresado con éxito"
+              else
+                     redirect_to posts_dashboard_path, notice: "El post no se pudo ingresar, intente nuevamente"
+              end
+       end
+
+       private
+              def post_params
+                     params.require(:post).permit(:title, :content, :image_url)
+              end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+       validates :title, presence: true
+       validates :content, presence:true
+       validates :image_url, presence: true
+
+end
